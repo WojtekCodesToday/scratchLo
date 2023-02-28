9918e6e9d294edcbefdca98991013226252246fa
WojtekGame <94251112+WojtekCodesToday@users.noreply.github.com>
2023-02-28 20:26:09 +0100

    Update app.vue

--- app.vue
+++ app.vue
@@ -1,5 +1,28 @@
 <template>
-  <div>
+<link rel="stylesheet" type="text/css" href="/global.css">
+  <div class="wrapper">
     <p>hello</p>
   </div>
+  <style scoped>
+      .wrapper {
+        position: sticky;
+        top: 0;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        flex-wrap: nowrap;
+        box-sizing: border-box;
+        height: 3rem;
+        font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+        font-size: 0.75rem;
+        font-weight: bold;
+        background-color: var(--header-background);
+        color: white;
+        border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+        box-shadow: 0 0 3px rgb(0 0 0 / 25%);
+        overflow: auto;
+        scrollbar-width: none;
+        z-index: 10;
+    }
+  </style>
 </template>
